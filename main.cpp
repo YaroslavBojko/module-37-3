05e5df7ceefdd2f4bd52db9896c650116d206df1
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2023-11-14 06:49:35 +0300

    add ui-file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <QApplication>
 #include <QPushButton>
+#include "./ui_console.h"
+
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    QPushButton button("Hello world!", nullptr);
-    button.resize(200, 100);
-    button.show();
+    QMainWindow window(nullptr);
+    Ui::MainWindow console;
+    console.setupUi(&window);
+    window.resize(240, 680);
+    window.show();
     return QApplication::exec();
 }
